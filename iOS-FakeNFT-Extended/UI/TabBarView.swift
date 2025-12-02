2f0a6a910a349212059758f9b38fc75e433a5ec0
vadimsuhanov <vadimsuhanov@gmail.com>
2025-12-02 20:41:07 +0300

    Assets were added

--- iOS-FakeNFT-Extended/UI/TabBarView.swift
+++ iOS-FakeNFT-Extended/UI/TabBarView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
 
 struct TabBarView: View {
+    init() {
+        UITabBar.appearance().unselectedItemTintColor = .ypBlack
+    }
     var body: some View {
         TabView {
             TestCatalogView()
                 .tabItem {
-                    Label(
-                        NSLocalizedString("Tab.catalog", comment: ""),
-                        systemImage: "square.stack.3d.up.fill"
-                    )
+                    Label(NSLocalizedString("Tab.catalog", comment: ""), image: .TabBarIcons.cart)
+                }
+                .backgroundStyle(.background)
+            IconsView()
+                .tabItem {
+                    Label(NSLocalizedString("Tab.statistics", comment: ""), image: .TabBarIcons.statistics)
                 }
                 .backgroundStyle(.background)
         }
