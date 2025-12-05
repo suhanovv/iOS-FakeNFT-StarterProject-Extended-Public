671b8650f8d391c71e1a64a41222ab52b9bfc6d0
vadimsuhanov <vadimsuhanov@gmail.com>
2025-12-05 19:24:04 +0300

    Добавил nft карточку #11

---
+++ iOS-FakeNFT-Extended/UI/Components/NftCard/NftCardView+ViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ViewModel.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Вадим Суханов on 05.12.2025.
+//
+
+import SwiftUI
+
+extension NftCardView {
+    @Observable
+    class ViewModel {
+        let nft: Nft
+        var image: URL {
+            nft.images[0]
+        }
+        private(set) var isLiked: Bool
+        private(set) var isInCart: Bool
+        
+        init(nft: Nft, isLiked: Bool, isInCart: Bool) {
+            self.nft = nft
+            self.isLiked = isLiked
+            self.isInCart = isInCart
+        }
+        
+        func likeTapped() {
+            isLiked.toggle()
+        }
+        
+        func cartTapped() {
+            isInCart.toggle()
+        }
+    }
+}
---
+++ iOS-FakeNFT-Extended/UI/Components/NftCard/NftCardView.swift
@@ -0,0 +1,60 @@
+//
+//  NftCardView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Вадим Суханов on 05.12.2025.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct NftCardView: View {
+    @State var viewModel: ViewModel
+    var body: some View {
+        VStack(alignment: .leading ,spacing: 8) {
+            KFImage(viewModel.image)
+                .placeholder {
+                    Rectangle()
+                        .foregroundStyle(.ypLightGray)
+                        .overlay {
+                            ProgressView()
+                        }
+                }
+                .resizable()
+                .scaledToFit()
+                .frame(width: 108, height: 108)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            VStack(alignment: .leading) {
+                RatingView(rating: viewModel.nft.rating)
+                HStack {
+                    VStack {
+                        Text(viewModel.nft.name)
+                            .font(.system(size: 17, weight: .bold))
+                            .lineLimit(1)
+                        Text(viewModel.nft.price, format: .currency(code: "ETH"))
+                            .font(.system(size: 10, weight: .medium))
+                    }
+                    Image(viewModel.isInCart ? .NftCardIcons.cartDelete : .NftCardIcons.cartAdd)
+                        .frame(width: 40, height: 40)
+                        .onTapGesture {
+                            viewModel.cartTapped()
+                        }
+                }
+            }
+        }
+        .overlay(alignment: .topTrailing) {
+            Image(.NftCardIcons.like)
+                .foregroundStyle(viewModel.isLiked ? .ypRedUniversal : .ypWhiteUniversal)
+                .frame(width: 40, height: 40)
+                .onTapGesture {
+                    viewModel.likeTapped()
+                }
+        }
+        .frame(width: 108, height: 192)
+    }
+}
+
+#Preview {
+    NftCardView(viewModel: NftCardView.ViewModel(nft: Nft.sampleCarmine, isLiked: false, isInCart: false))
+    NftCardView(viewModel: NftCardView.ViewModel(nft: Nft.sampleDominique, isLiked: true, isInCart: true))
+}
