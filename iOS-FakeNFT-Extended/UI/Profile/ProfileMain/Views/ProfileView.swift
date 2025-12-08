accc2ea941a6c8aecd7b586b5a72756151fa5f7f
Korol <korolevadidan@gmail.com>
2025-12-08 21:56:25 +0700

    [fix] исправлен коммит

---
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileMain/Views/ProfileView.swift
@@ -0,0 +1,105 @@
+//
+//  ProfileView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Ди Di on 08/12/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var photoURL: URL?
+    var name: String
+    var description: String
+    var website: String
+    @State private var isEditing = false
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center, spacing: 16) {
+                ProfilePhotoView(url: photoURL)
+                nameRow
+                Spacer()
+            }
+            .padding(.vertical, 20)
+            
+            descriptionRow
+            webRow
+            listRows
+        }
+        .padding(.horizontal, 16)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(isPresented: $isEditing) {
+            ProfileEditView(
+                name: name,
+                description: description,
+                website: website,
+                photoURL: photoURL
+            )
+        }
+        .toolbar {
+            navigationToolbar
+        }
+    }
+    
+    // MARK: - Views
+    private var nameRow: some View {
+        Text(name)
+            .font(Font(UIFont.headline3))
+            .foregroundColor(.ypBlack)
+    }
+    
+    private var descriptionRow: some View {
+        Text(description)
+            .font(Font(UIFont.caption2))
+            .foregroundColor(.ypBlack)
+            .padding(.bottom, 8)
+    }
+    
+    private var webRow: some View {
+        Link(website, destination: URL(string: "https://\(website)")!)
+            .font(Font(UIFont.caption1))
+            .foregroundColor(.ypBlueUniversal)
+            .underline()
+    }
+    
+    private var listRows: some View {
+        List {
+            ProfileListRow(title: "Мои NFT", count: 21) {
+                // add action
+            }
+            
+            ProfileListRow(title: "Избранные NFT", count: 21) {
+                // add action
+            }
+        }
+        .listStyle(.plain)
+        .scrollContentBackground(.hidden)
+        .padding(.top, 40)
+    }
+    
+    // MARK: - Toolbar
+    private var navigationToolbar: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button {
+                isEditing = true
+            } label: {
+                Image(systemName: "square.and.pencil")
+                    .font(.system(size: 26))
+                    .foregroundColor(.ypBlack)
+            }
+        }
+    }
+}
+
+// MARK: - Preview_ProfileView
+#Preview {
+    NavigationStack {
+        ProfileView(
+            photoURL: URL(string: "https://picsum.photos/id/237/200/200"),
+            name: "Joaquin Phoenix",
+            description: "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям.",
+            website: "JoaquinPhoenix.com"
+        )
+    }
+}
