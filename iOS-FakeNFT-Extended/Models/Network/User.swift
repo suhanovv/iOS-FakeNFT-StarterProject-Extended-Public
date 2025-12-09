27521e4861a2e47b379afc7d2a6af97833011a8c
vadimsuhanov <vadimsuhanov@gmail.com>
2025-12-09 21:05:45 +0300

    users screen was added

--- iOS-FakeNFT-Extended/Models/Network/User.swift
+++ iOS-FakeNFT-Extended/Models/Network/User.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct User: Decodable {
+struct User: Decodable, Identifiable {
     let name: String
     let avatar: URL
     let description: String
@@ -8,4 +8,23 @@ struct User: Decodable {
     let nfts: [String]
     let rating: Int
     let id: String
+}
+
+extension User {
+    static func makeFakeUser() -> User {
+        
+        let avatars = [
+            "https://n1s2.hsmedia.ru/10/07/5b/10075bc9f87787e109c8bd9d93e8d66b/600x400_0x0a330c9a_8308133731545062329.jpeg",
+            "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+            "https://i.pinimg.com/736x/07/41/63/0741631fc2493022c067e3a38528a5c2.jpg"
+        ]
+        return User(
+            name: ["Joaquin Phoenix", "Bill", "Elon Musk", "Kanye West"].randomElement()!,
+            avatar: URL(string: avatars.randomElement()!)!,
+            description: "With a keen eye for emerging trends and a commitment to authenticity, I offer a diverse collection of digital assets that cater to collectors and investors alike.",
+            website: URL(string: "https://practicum.yandex.ru/graphic-designer/")!,
+            nfts: [],
+            rating: Int.random(in: 1...50),
+            id: UUID().uuidString)
+    }
 }
--- iOS-FakeNFT-Extended/UI/TabBarView.swift
+++ iOS-FakeNFT-Extended/UI/TabBarView.swift
@@ -1,21 +1,38 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
     init() {
-        UITabBar.appearance().unselectedItemTintColor = .ypBlack
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+
+        appearance.stackedLayoutAppearance.normal.iconColor = .ypBlack
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor(resource: .ypBlack)]
+        appearance.stackedLayoutAppearance.selected.iconColor = .ypBlueUniversal
+
+        UITabBar.appearance().standardAppearance = appearance
+            
     }
+    
     var body: some View {
-        TabView {
-            TestCatalogView()
-                .tabItem {
-                    Label(NSLocalizedString("Tab.catalog", comment: ""), image: .TabBarIcons.cart)
-                }
-                .backgroundStyle(.background)
-            IconsView()
-                .tabItem {
-                    Label(NSLocalizedString("Tab.statistics", comment: ""), image: .TabBarIcons.statistics)
-                }
-                .backgroundStyle(.background)
+        NavigationStack {
+            TabView {
+                TestCatalogView()
+                    .tabItem {
+                        Label(Constants.tabCatalogTitle, image: .TabBarIcons.cart)
+                    }
+                    .backgroundStyle(.ypWhite)
+                StatisticsScreenView()
+                    .tabItem {
+                        Label(Constants.tabStatisticsTitle, image: .TabBarIcons.statistics)
+                    }
+                    .background(.ypWhite)
+            }
         }
     }
+}
+
+private enum Constants {
+    static let tabCatalogTitle = NSLocalizedString("Tab.catalog", comment: "")
+    static let tabStatisticsTitle = NSLocalizedString("Tab.statistics", comment: "")
 }
