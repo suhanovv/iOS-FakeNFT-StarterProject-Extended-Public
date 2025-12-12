2b9e3e04d1d8e88c1c60ba5516a5b935c366830d
vadimsuhanov <vadimsuhanov@gmail.com>
2025-12-12 21:09:27 +0300

    service for users was added

--- iOS-FakeNFT-Extended/Services/Users/UsersService.swift
+++ iOS-FakeNFT-Extended/Services/Users/UsersService.swift
@@ -5,7 +5,26 @@ enum UsersSortBy: String {
     case rating
 }
 
-protocol UsersServiceProtocol {
-    func getUsers(page: Int?, orderBy: UsersSortBy?) async throws -> [User]
+protocol UsersServiceProtocol: Sendable {
+    func getUsers(page: Int?, orderBy: UsersSortBy) async throws -> [User]
     func getUserById(_ userId: String) async throws -> User
+}
+
+actor UsersService: UsersServiceProtocol {
+    private let networkClient: NetworkClient
+    private let perPage = 20
+    
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+    
+    func getUsers(page: Int?, orderBy: UsersSortBy) async throws -> [User] {
+        let request = GetUsersRequest(page: page ?? 0, size: perPage, sortBy: orderBy.rawValue)
+        return try await networkClient.send(request: request)
+    }
+
+    func getUserById(_ userId: String) async throws -> User {
+        let request = GetUserByIdRequest(userId: userId)
+        return try await networkClient.send(request: request)
+    }
 }
