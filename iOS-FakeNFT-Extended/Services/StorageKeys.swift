d6de430329a41efc860c506dae58b1c670e269cd
Korol <korolevadidan@gmail.com>
2025-12-17 22:40:39 +0700

    [refactor] Edits added after review

---
+++ iOS-FakeNFT-Extended/Services/StorageKeys.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Ди Di on 12/12/25.
+//
+
+import Foundation
+
+enum StorageKeys {
+        static let name = "profile_name"
+        static let description = "profile_description"
+        static let website = "profile_website"
+        static let photoURL = "profile_photo_url"
+        static let favouriteNFTIds = "favourite_nft_ids"
+}
