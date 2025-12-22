dbf6ec45c318090798b6627308f9c7d1f0b89341
Diana Viter <>
2025-12-22 16:13:06 +0300

    fix a switch case

--- iOS-FakeNFT-Extended/UI/Catalogue/CatalogueScreen/CatalogueView.swift
+++ iOS-FakeNFT-Extended/UI/Catalogue/CatalogueScreen/CatalogueView.swift
@@ -76,7 +76,7 @@ struct CatalogueView: View {
     @ViewBuilder
     private var collectionsScrollView: some View {
         switch viewModel.state {
-        case .loading:
+        case .initial, .loading:
             ProgressView()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             
