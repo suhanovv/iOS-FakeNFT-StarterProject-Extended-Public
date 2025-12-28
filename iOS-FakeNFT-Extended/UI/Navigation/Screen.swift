150037f58e6decfae00ee0f856afe7216e5ef411
Korol <korolevadidan@gmail.com>
2025-12-28 12:00:09 +0700

    Restore Screen enum with profileEdit and NFT routes

--- iOS-FakeNFT-Extended/UI/Navigation/Screen.swift
+++ iOS-FakeNFT-Extended/UI/Navigation/Screen.swift
@@ -1,19 +1,13 @@
-//
-//  Screen.swift
-//  iOS-FakeNFT-Extended
-//
-//  Created by Вадим Суханов on 11.12.2025.
-//
-
 import Foundation
 
 enum Screen: Hashable {
     case main
-    case usersList
     case profile
+    case profileEdit
+    case myNft
+    case favouriteNft
+    case usersList
     case userCard(userId: String)
     case userCollection(userId: String)
     case webView(url: URL)
-    case collection(id: String)
-    case catalogue
 }
